f375602db77d2378913cfe7b0f1504b7c5c95a4e
eedu7 <mueedahmad067@gmail.com>
2025-04-04 23:15:47 +0500

    Added the List Button

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -7,7 +7,7 @@ import {
     AlignLeftIcon, AlignRightIcon,
     BoldIcon,
     ChevronDownIcon, HighlighterIcon, ImageIcon,
-    ItalicIcon, Link2Icon,
+    ItalicIcon, Link2Icon, ListIcon, ListOrderedIcon,
     ListTodoIcon,
     LucideIcon,
     MessageSquarePlusIcon,
@@ -26,6 +26,56 @@ import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger
 import {Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle} from "@/components/ui/dialog";
 import {Button} from "@/components/ui/button";
 import {Input} from "@/components/ui/input";
+
+const ListButton = () => {
+    const {editor} = useEditorStore();
+
+    const lists: {label: string, icon: LucideIcon, isActive: () => boolean | undefined, onClick?: () => void}[] = [
+        {
+            label: "Bullet List",
+            icon: ListIcon,
+            isActive: () => editor?.isActive("bulletList"),
+            onClick: () => editor?.chain().focus().toggleBulletList().run()
+        },
+        {
+            label: "Ordered List",
+            icon: ListOrderedIcon,
+            isActive: () => editor?.isActive("orderedList"),
+            onClick: () => editor?.chain().focus().toggleOrderedList().run()
+        },
+
+    ]
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                    className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                    <ListIcon className="size-4" />
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+                {
+                    lists.map(({label, icon: Icon, onClick, isActive}) => (
+                        <button
+                            key={label}
+                            onClick={onClick}
+                            className={cn(
+                                "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                                isActive() && "bg-neutral-200/80"
+                            )}
+                        >
+                            <Icon className="size-4" />
+                            <span className="text-sm">{label}</span>
+                        </button>
+                    ))
+                }
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
 
 const AlignButton = () => {
     const {editor} = useEditorStore();
@@ -465,7 +515,7 @@ export const Toolbar = () => {
         <ImageButton />
         <AlignButton />
         {/* TODO: Line Height */}
-        {/* TODO: List */}
+        <ListButton />
         {sections[2].map((item) => (<ToolbarButton key={item.label} {...item} />))}
     </div>)
 }
