e1cf10e19d2fe4f9de7a1521622aca3feff6ff30
eedu7 <mueedahmad067@gmail.com>
2025-04-04 15:59:59 +0500

    Added the spellcheck button

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React from 'react'
-import {LucideIcon, Undo2Icon} from "lucide-react";
+import {LucideIcon, PrinterIcon, Redo2Icon, SpellCheckIcon, Undo2Icon} from "lucide-react";
 import {cn} from "@/lib/utils";
 import {useEditorStore} from "@/store/use-editor-store";
 
@@ -23,16 +23,22 @@ export const Toolbar = () => {
 
     const {editor} = useEditorStore();
 
-    console.log("Toolbar editor: ", {editor});
-
-    const sections: { label: string, icon: LucideIcon, onclick: () => void, isActive?: boolean }[][] = [[{
-        label: "Undo", icon: Undo2Icon, onclick: () => {
-            editor?.chain().focus().undo().run()
+    const sections: { label: string, icon: LucideIcon, onClick: () => void, isActive?: boolean }[][] = [[{
+        label: "Undo", icon: Undo2Icon, onClick: () => editor?.chain().focus().undo().run()
+    }, {
+        label: "Redo", icon: Redo2Icon, onClick: () => editor?.chain().focus().redo().run()
+    }, {
+        label: "Print", icon: PrinterIcon, onClick: () => window.print()
+    }, {
+        label: "Spell Check", icon: SpellCheckIcon, onClick: () => {
+            // Using the pure browser api to spellcheck, like setting html attribute spellcheck `true` or `false`
+            const current = editor?.view.dom.getAttribute("spellcheck");
+            editor?.view.dom.setAttribute("spellcheck", current === "false" ? "true" : "false");
         }
     }]];
 
     return (<div
         className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
-        {sections[0].map((item) => (<ToolbarButton key={item.label} onClick={item.onclick} {...item} />))}
+        {sections[0].map((item) => (<ToolbarButton key={item.label} {...item} />))}
     </div>)
 }
