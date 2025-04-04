37888dbf4a664930871d270b0e84163d1a30126e
eedu7 <mueedahmad067@gmail.com>
2025-04-04 20:41:38 +0500

    Added the font heading

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -15,52 +15,111 @@ import {
     UnderlineIcon,
     Undo2Icon
 } from "lucide-react";
+import {Level} from "@tiptap/extension-heading";
 import {cn} from "@/lib/utils";
 import {useEditorStore} from "@/store/use-editor-store";
 import {Separator} from "@/components/ui/separator";
 import {DropdownMenu, DropdownMenuContent, DropdownMenuTrigger} from "@/components/ui/dropdown-menu"
 
+const HeadingLevelButton = () => {
+    const {editor} = useEditorStore();
+
+    const headings: { label: string, value: number, fontSize: string }[] = [
+        {label: "Normal Text", value: 0, fontSize: "16px"},
+        {label: "Heading 1", value: 1, fontSize: "32px"},
+        {label: "Heading 2", value: 2, fontSize: "24px"},
+        {label: "Heading 3", value: 3, fontSize: "20px"},
+        {label: "Heading 4", value: 4, fontSize: "18px"},
+        {label: "Heading 5", value: 5, fontSize: "16px"},
+    ]
+
+    const getCurrentHeading = () => {
+        for (let level = 1; level <= 5; level++) {
+            if (editor?.isActive("heading", { level })) {
+                return `Heading ${level}`;
+            }
+        }
+        return "Normal text";
+    }
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                    className="h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+                    <span className="truncate">
+                        {getCurrentHeading()}
+                    </span>
+                    <ChevronDownIcon className="ml-2 size-4 shrink-0"/>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+                {
+                    headings.map(({label, value, fontSize}) => (
+                        <button
+                        key={value}
+                        style={{ fontSize }}
+                        onClick={() => {
+                            if (value === 0){
+                                editor?.chain().focus().setParagraph().run()
+                            } else {
+                                editor?.chain().focus().toggleHeading({ level: value as Level }).run()
+                            }
+
+                        }}
+                        className={
+                            cn(
+                                "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                                (value === 0 && !editor?.isActive("heading") || editor?.isActive("heading", { level: value })) && "bg-neutral-200/80"
+                            )
+                        }
+                        >
+                            <span style={{fontSize}}>{label}</span>
+                        </button>
+                    ))
+                }
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
 const FontFamilyButton = () => {
     const {editor} = useEditorStore();
 
-    const fonts: { label: string, value: string }[] = [{label: "Arial", value: "Arial"}, {
-        label: "Arial Black",
-        value: "Arial Black"
-    }, {label: "Verdana", value: "Verdana"}, {label: "Tahoma", value: "Tahoma"}, {
-        label: "Trebuchet MS",
-        value: "Trebuchet MS"
-    }, {label: "Impact", value: "Impact"}, {label: "Times New Roman", value: "Times New Roman"}, {
-        label: "Georgia",
-        value: "Georgia"
-    }, {label: "Courier New", value: "Courier New"}, {
-        label: "Lucida Console",
-        value: "Lucida Console"
-    }, {label: "Lucida Sans Unicode", value: "Lucida Sans Unicode"}, {
-        label: "Palatino Linotype",
-        value: "Palatino Linotype"
-    }, {label: "Garamond", value: "Garamond"}, {
-        label: "Comic Sans MS",
-        value: "Comic Sans MS"
-    }, {label: "Franklin Gothic Medium", value: "Franklin Gothic Medium"}, {
-        label: "Century Gothic",
-        value: "Century Gothic"
-    }, {label: "Bookman Old Style", value: "Bookman Old Style"}, {
-        label: "Candara",
-        value: "Candara"
-    }, {label: "Consolas", value: "Consolas"}, {label: "Copperplate", value: "Copperplate"}, {
-        label: "Futura",
-        value: "Futura"
-    }, {label: "Geneva", value: "Geneva"}, {label: "Rockwell", value: "Rockwell"}, {
-        label: "Segoe UI",
-        value: "Segoe UI"
-    }, {label: "Perpetua", value: "Perpetua"}, {label: "Monaco", value: "Monaco"}];
+    const fonts: { label: string, value: string }[] = [
+        { label: "Arial", value: "Arial"},
+        { label: "Arial Black", value: "Arial Black"},
+        { label: "Verdana", value: "Verdana"},
+        { label: "Tahoma", value: "Tahoma"},
+        { label: "Trebuchet MS", value: "Trebuchet MS"},
+        { label: "Impact", value: "Impact"},
+        { label: "Times New Roman", value: "Times New Roman"},
+        { label: "Georgia", value: "Georgia"},
+        { label: "Courier New", value: "Courier New"},
+        { label: "Lucida Console", value: "Lucida Console"},
+        { label: "Lucida Sans Unicode", value: "Lucida Sans Unicode"},
+        { label: "Palatino Linotype", value: "Palatino Linotype"},
+        { label: "Garamond", value: "Garamond"},
+        { label: "Comic Sans MS", value: "Comic Sans MS"},
+        { label: "Franklin Gothic Medium", value: "Franklin Gothic Medium"},
+        { label: "Century Gothic", value: "Century Gothic"},
+        { label: "Bookman Old Style", value: "Bookman Old Style"},
+        { label: "Candara", value: "Candara"},
+        { label: "Consolas", value: "Consolas"},
+        { label: "Copperplate", value: "Copperplate"},
+        { label: "Futura", value: "Futura"},
+        { label: "Geneva", value: "Geneva"},
+        { label: "Rockwell", value: "Rockwell"},
+        { label: "Segoe UI", value: "Segoe UI"},
+        { label: "Perpetua", value: "Perpetua"},
+        { label: "Monaco", value: "Monaco"}];
 
     return (<DropdownMenu>
         <DropdownMenuTrigger asChild>
             <button
                 className="h-7 w-[120px] shrink-0 flex items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
                     <span className="truncate">
-                    {editor?.getAttributes("textStyle").fontFamily || "Arial"}
+                        {editor?.getAttributes("textStyle").fontFamily || "Arial"}
                     </span>
                 <ChevronDownIcon className="ml-2 size-4 shrink-0"/>
             </button>
@@ -71,8 +130,14 @@ const FontFamilyButton = () => {
                     type="button"
                     key={value}
                     onClick={() => editor?.chain().focus().setFontFamily(value).run()}
-                    className={cn("flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80", editor?.getAttributes("textStyle").fontfamily === value && "bg-neutral-200/80")}
-                    style={{fontFamily: value}}>
+                    className={
+                        cn(
+                            "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                            editor?.getAttributes("textStyle").fontfamily === value && "bg-neutral-200/80"
+                        )
+                    }
+                    style={{fontFamily: value}}
+                >
                     <span className="text-sm">{label}</span>
                 </button>
             ))}
@@ -145,7 +210,7 @@ export const Toolbar = () => {
         <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
         <FontFamilyButton/>
         <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
-        {/* TODO: Heading */}
+        <HeadingLevelButton />
         <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
         {/* TODO: FontSize */}
         <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
