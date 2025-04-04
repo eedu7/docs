00d98bccd6224fbe32d2fc82708ce027ebbb90fa
eedu7 <mueedahmad067@gmail.com>
2025-04-04 15:47:56 +0500

    Updating the setEditor state, when any type of change occurs on the tiptap editor,

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -11,11 +11,30 @@ import TableRow from '@tiptap/extension-table-row'
 import Image from '@tiptap/extension-image'
 import ImageResize from "tiptap-extension-resize-image"
 
+import {useEditorStore} from "@/store/use-editor-store";
 
 export const Editor = () => {
 
+    const {setEditor} = useEditorStore();
+
     const editor = useEditor({
-        editorProps: {
+        onCreate({editor}) {
+            setEditor(editor)
+        }, onDestroy() {
+            setEditor(null)
+        }, onUpdate({editor}) {
+            setEditor(editor)
+        }, onSelectionUpdate({editor}) {
+            setEditor(editor)
+        }, onTransaction({editor}) {
+            setEditor(editor)
+        }, onFocus({editor}) {
+            setEditor(editor)
+        }, onBlur({editor}) {
+            setEditor(editor)
+        }, onContentError({editor}) {
+            setEditor(editor)
+        }, editorProps: {
             attributes: {
                 style: "padding-left: 56px; padding-right: 56px;",
                 class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import {LucideIcon, Undo2Icon} from "lucide-react";
 import {cn} from "@/lib/utils";
+import {useEditorStore} from "@/store/use-editor-store";
 
 interface ToolbarButtonProps {
     onClick?: () => void;
@@ -11,31 +12,27 @@ interface ToolbarButtonProps {
 }
 
 const ToolbarButton = ({onClick, isActive, icon: Icon}: ToolbarButtonProps) => {
-    return (<button onClick={onClick}
+    return (<button type="button" onClick={onClick}
                     className={cn("text-sm h-7 min-w-7 flex items-center  justify-center rounded-sm hover:bg-neutral-200/80", isActive && "bg-neutral-200/80")}>
-            <Icon className="size-4"/>
-        </button>)
+        <Icon className="size-4"/>
+    </button>)
 }
 
 
 export const Toolbar = () => {
 
-    const sections: { label: string, icon: LucideIcon, onclick: () => void, isActive?: boolean }[][] = [
-        [
-            {
-                label: "Undo",
-                icon: Undo2Icon,
-                onclick: () => console.log("Undo clicked!"),
-            }
-        ]
-    ];
+    const {editor} = useEditorStore();
+
+    console.log("Toolbar editor: ", {editor});
+
+    const sections: { label: string, icon: LucideIcon, onclick: () => void, isActive?: boolean }[][] = [[{
+        label: "Undo", icon: Undo2Icon, onclick: () => {
+            editor?.chain().focus().undo().run()
+        }
+    }]];
 
     return (<div
         className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
-        {
-            sections[0].map((item) => (
-                <ToolbarButton key={item.label} {...item} />
-            ))
-        }
+        {sections[0].map((item) => (<ToolbarButton key={item.label} onClick={item.onclick} {...item} />))}
     </div>)
 }
