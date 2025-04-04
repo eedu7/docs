650ac1ff771fa77f0191232e3552d96d71f8039e
eedu7 <mueedahmad067@gmail.com>
2025-04-04 13:12:27 +0500

    Added the task list

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,8 @@
 
 import {EditorContent, useEditor} from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
 
 
 export const Editor = () => {
@@ -12,7 +14,9 @@ export const Editor = () => {
                 style: "padding-left: 56px; padding-right: 56px;",
                 class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text"
             }
-        }, extensions: [StarterKit], content: "<p>Hello, World! 🌍</p>"
+        }, extensions: [StarterKit, TaskList, TaskItem.configure({
+            nested: true
+        })], content: "<p>Hello, World! 🌍</p>"
 
     })
 
