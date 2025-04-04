62715b5eea9d13752771ac48f2926b373e9ae880
eedu7 <mueedahmad067@gmail.com>
2025-04-04 15:14:40 +0500

    added the toolbar, and toolbar button props and also added the undo button (without any functionality)

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {Editor} from "@/app/documents/[documentId]/editor";
+import {Toolbar} from "@/app/documents/[documentId]/toolbar";
 
 interface DocumentIDPageProps {
     props: Promise<{ documentId: string }>
 }
 
 function DocumentIDPage({props}: DocumentIDPageProps) {
-    return (<div className="min-h-screen bg-[#FAFBFD]"><Editor/></div>)
+    return (<div className="min-h-screen bg-[#FAFBFD]">
+        <Toolbar/>
+        <Editor/></div>)
 }
 
 export default DocumentIDPage
