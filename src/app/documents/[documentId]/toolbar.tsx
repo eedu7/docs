b79f39e54f3d4f6d1a2e5b84e6c3f3d8a0a92871
eedu7 <mueedahmad067@gmail.com>
2025-04-04 21:26:55 +0500

    Added the highlight feature

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { type ColorResult, SketchPicker } from "react-color"
 import {
     BoldIcon,
-    ChevronDownIcon,
+    ChevronDownIcon, HighlighterIcon,
     ItalicIcon,
     ListTodoIcon,
     LucideIcon,
@@ -21,6 +21,35 @@ import {cn} from "@/lib/utils";
 import {useEditorStore} from "@/store/use-editor-store";
 import {Separator} from "@/components/ui/separator";
 import {DropdownMenu, DropdownMenuContent, DropdownMenuTrigger} from "@/components/ui/dropdown-menu"
+
+const HighLightColorButton = () => {
+    const {editor} = useEditorStore();
+
+    const value = editor?.getAttributes("highlight").color || "#FFFFFF";
+
+    const onChange = (color: ColorResult) => {
+        editor?.chain().focus().setHighlight({color: color.hex}).run();
+    }
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <button
+                    className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+                >
+                    <HighlighterIcon className="size-4" />
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="p-0 border-0 ">
+                <SketchPicker
+                    className="size-full"
+                    color={value}
+                    onChange={onChange}
+                />
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
 
 const TextColorButton = () => {
     const {editor} = useEditorStore();
@@ -248,7 +277,7 @@ export const Toolbar = () => {
         <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
         {sections[1].map((item) => (<ToolbarButton key={item.label} {...item} />))}
         <TextColorButton />
-        {/* TODO: Highlight color */}
+        <HighLightColorButton />
         <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
         {/* TODO: Link */}
         {/* TODO: Image */}
