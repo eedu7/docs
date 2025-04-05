1ea785695ea354954eaae6a0e49b03ad38759b92
eedu7 <mueedahmad067@gmail.com>
2025-04-05 12:36:02 +0500

    Added the menubar, and an option to save file, but without any functionality only ui

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
+import {DocumentInput} from "@/app/documents/[documentId]/document-input";
+
+import {Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger,} from "@/components/ui/menubar"
+import {FileIcon} from "lucide-react";
 
 
 export const Navbar = () => {
@@ -10,8 +14,23 @@ export const Navbar = () => {
                     <Image src="/logo.svg" alt="Logo" width={36} height={36}/>
                 </Link>
                 <div className="flex flex-col">
-                    {/* TODO: DocumentInput */}
-                    {/* TODO: Menubar */}
+                    <DocumentInput/>
+                    <div className="flex">
+                        <Menubar className="border-none bg-transparent shadow-none h-auto p-0">
+                            <MenubarMenu>
+                                <MenubarTrigger>
+                                    File
+                                </MenubarTrigger>
+                                <MenubarContent>
+                                    <MenubarItem>
+                                        <FileIcon className="size-4 mr-2" />
+                                        Save
+                                    </MenubarItem>
+                                </MenubarContent>
+                            </MenubarMenu>
+
+                        </Menubar>
+                    </div>
                 </div>
             </div>
         </nav>)
