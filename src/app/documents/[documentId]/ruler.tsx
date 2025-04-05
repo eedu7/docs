d798f652a0c35a865f3b17f2b30a768b2448c66e
eedu7 <mueedahmad067@gmail.com>
2025-04-05 12:20:33 +0500

    Added the margin

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -1,12 +1,79 @@
+"use client";
+
 import React from 'react'
+
+import {FaCaretDown} from "react-icons/fa"
+
 
 const markers = Array.from({length: 83}, (_, i) => i)
 
 export const Ruler = () => {
+
+    const [leftMargin, setLeftMargin] = React.useState(56);
+    const [rightMargin, setRightMargin] = React.useState(56);
+
+    const [isDraggingLeft, setIsDraggingLeft] = React.useState(false);
+    const [isDraggingRight, setIsDraggingRight] = React.useState(false);
+
+    const rulerRef = React.useRef<HTMLDivElement>(null);
+
+    const handleLeftMouseDown = () => {
+        setIsDraggingLeft(true);
+    }
+
+    const handleRightMouseDown = () => {
+        setIsDraggingRight(true);
+    }
+
+    const handleMouseMove = (e: React.MouseEvent) => {
+        const PAGE_WIDTH: number = 816;
+        const MINIMUM_SPACE: number = 100;
+
+        if ((isDraggingLeft || isDraggingRight) && rulerRef.current) {
+            const container = rulerRef.current.querySelector("#ruler-container");
+
+            if (container) {
+                const containerRect = container.getBoundingClientRect();
+                const relativeX = e.clientX - containerRect.left;
+                const rawPosition = Math.max(0, Math.min(PAGE_WIDTH, relativeX));
+
+                if (isDraggingLeft) {
+                    const maxLeftPosition = PAGE_WIDTH - rightMargin - MINIMUM_SPACE;
+                    const newLeftPosition = Math.min(rawPosition, maxLeftPosition);
+                    setLeftMargin(newLeftPosition);  // TODO: Make collaborative
+                } else if (isDraggingRight) {
+                    const maxRightPosition = PAGE_WIDTH - leftMargin - MINIMUM_SPACE;
+                    const newRightPosition = Math.max(PAGE_WIDTH - rawPosition, 0)
+                    const constrainedRightPosition = Math.min(newRightPosition, maxRightPosition)
+                    setRightMargin(constrainedRightPosition);
+
+                }
+            }
+        }
+    }
+    const handleMouseUp = () => {
+        setIsDraggingLeft(false);
+        setIsDraggingRight(false);
+    }
+
+    const handleLeftDoubleClick = () => {
+        setLeftMargin(56);
+    }
+    const handleRightDoubleClick = () => {
+        setRightMargin(56);
+    }
+
     return (
-        <div className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
+        <div
+            ref={rulerRef}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
+            onMouseLeave={handleMouseUp}
+            className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
             <div id="ruler-container"
                  className="max-w-[816px] mx-auto w-full h-full relative">
+                <Marker position={leftMargin} isLeft={true} isDragging={isDraggingLeft} onMouseDown={handleLeftMouseDown} onDoubleClick={handleLeftDoubleClick}/>
+                <Marker position={rightMargin} isLeft={false} isDragging={isDraggingRight} onMouseDown={handleRightMouseDown} onDoubleClick={handleRightDoubleClick}/>
                 <div className="absolute inset-x-0 bottom-0 h-full">
                     <div className="relative h-full w-[816px]">
                         {
@@ -42,3 +109,59 @@ export const Ruler = () => {
         </div>
     )
 }
+
+
+interface MarkerProps {
+    position: number;
+    isLeft: boolean;
+    isDragging: boolean;
+    onMouseDown: () => void;
+    onDoubleClick: () => void
+}
+
+const Marker = ({position, isLeft, isDragging, onMouseDown, onDoubleClick}: MarkerProps) =>{
+    return (
+        <div className="absolute top-0 w-4 h-full cursor-ew-resize z-[5] group -ml-2"
+            style={{ [isLeft ? "left" : "right"]: `${position}px` }}
+             onMouseDown={onMouseDown}
+             onDoubleClick={onDoubleClick}
+        >
+            <FaCaretDown className="absolute left-1/2 top-0 h-full fill-blue-500 transform -translate-x-1/2" />
+            <div
+            className="absolute left-1/2 top-4 transform -translate-x-1/2"
+            style={{
+                height: "100vh",
+                width: "1px",
+                transform: "scaleX(0.5)",
+                backgroundColor: "#3B72F6",
+                display: isDragging ? "block" : "none",
+            }}
+            />
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
