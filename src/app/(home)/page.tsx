501bc192a8f70b472a34a464c6c0b4bbea13e1d9
eedu7 <mueedahmad067@gmail.com>
2025-04-05 16:17:18 +0500

    added the navbar component

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import {Navbar} from "@/app/(home)/navbar";
+
+
+
+export default function Home() {
+    return (
+        <div className="flex min-h-screen flex-col">
+            <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
+                <Navbar />
+            </div>
+            <div className="mt-16">
+                Click <Link href="/documents/123"><span className="text-blue-800 underline">here</span></Link>, to go to
+                document
+            </div>
+        </div>
+    );
+}
--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-    return (<div className="flex min-h-screen justify-center items-center">
-        Click <Link href="/documents/123"><span className="text-blue-800 underline ml-2">here</span></Link>, to go to
-        document
-    </div>);
-}
