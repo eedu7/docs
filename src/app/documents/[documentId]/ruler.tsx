afaed7fe83fc5dd86af7f438a126c533f335ccd8
eedu7 <mueedahmad067@gmail.com>
2025-04-05 13:14:12 +0500

    FIXES: added the fix for the right ruler, as it was not positioned very well, when zoomed in

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -69,9 +69,9 @@ export const Ruler = () => {
             onMouseMove={handleMouseMove}
             onMouseUp={handleMouseUp}
             onMouseLeave={handleMouseUp}
-            className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
+            className="w-[816px] mx-auto h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
             <div id="ruler-container"
-                 className="max-w-[816px] mx-auto w-full h-full relative">
+                 className="w-full h-full relative">
                 <Marker position={leftMargin} isLeft={true} isDragging={isDraggingLeft} onMouseDown={handleLeftMouseDown} onDoubleClick={handleLeftDoubleClick}/>
                 <Marker position={rightMargin} isLeft={false} isDragging={isDraggingRight} onMouseDown={handleRightMouseDown} onDoubleClick={handleRightDoubleClick}/>
                 <div className="absolute inset-x-0 bottom-0 h-full">
