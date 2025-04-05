47b26d0132d9dad35264f505f46a2b5bc0ed9f83
eedu7 <mueedahmad067@gmail.com>
2025-04-05 20:08:11 +0500

    Added the documents schema and also index fields

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import {Navbar} from "@/app/(home)/navbar";
 import {TemplatesGallery} from "@/app/(home)/templates-gallery";
-
+import {useQuery} from "convex/react";
+import {api} from "../../../convex/_generated/api";
 
 export default function Home() {
+
+    const documents = useQuery(api.documents.get)
+
+    if (documents === undefined) {
+        return (
+            <p>Loading...</p>
+        )
+    }
+
     return (
         <div className="flex min-h-screen flex-col">
             <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -10,6 +22,11 @@ export default function Home() {
             </div>
             <div className="mt-16">
                 <TemplatesGallery/>
+                {
+                    documents?.map(({title, _id}) => (
+                        <span key={_id}>{title}</span>
+                    ))
+                }
             </div>
         </div>);
 }
