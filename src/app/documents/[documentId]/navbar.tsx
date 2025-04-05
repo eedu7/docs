f2b93996b825498600bc18a3b608e5bbd2ed5f03
eedu7 <mueedahmad067@gmail.com>
2025-04-05 15:56:32 +0500

    Added the functionality to save document in html, json, text and pdf

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -36,6 +36,44 @@ export const Navbar = () => {
         editor?.chain().focus().insertTable({rows, cols, withHeaderRow: false}).run()
     }
 
+    const onDownload = (blog: Blob, filename: string) => {
+        const url = URL.createObjectURL(blog);
+        const a =  document.createElement("a");
+        a.href = url;
+        a.download = filename;
+        a.click();
+    }
+
+    const onSaveJSON = () => {
+        if (!editor) return;
+
+        const content = editor.getJSON()
+        const blob= new Blob([JSON.stringify(content)], {
+            type: "application/json",
+        })
+        onDownload(blob, "document.json")  // TODO: Use document name
+    }
+
+    const onSaveHTML = () => {
+        if (!editor) return;
+
+        const content = editor.getHTML()
+        const blob= new Blob([content], {
+            type: "text/html",
+        })
+        onDownload(blob, "document.html")  // TODO: Use document name
+    }
+
+    const onSaveText = () => {
+        if (!editor) return;
+
+        const content = editor.getText()
+        const blob= new Blob([content], {
+            type: "text/plain",
+        })
+        onDownload(blob, "document.txt")  // TODO: Use document name
+    }
+
     return (
         <nav className="flex items-center justify-between">
             <div className="flex gap-2 items-center">
@@ -57,23 +95,19 @@ export const Navbar = () => {
                                         Save
                                         </MenubarSubTrigger>
                                         <MenubarSubContent>
-                                            <MenubarItem>
+                                            <MenubarItem onClick={onSaveJSON}>
                                                 <FileJsonIcon className="size-4 mr-2" />
                                                 JSON
                                             </MenubarItem>
-                                            <MenubarItem>
+                                            <MenubarItem onClick={onSaveHTML}>
                                                 <GlobeIcon className="size-4 mr-2" />
                                                 HTML
                                             </MenubarItem>
-                                            <MenubarItem>
+                                            <MenubarItem onClick={() => window.print()}>
                                                 <BsFilePdf className="size-4 mr-2" />
                                                 PDF
                                             </MenubarItem>
-                                            <MenubarItem>
-                                                <FileJsonIcon className="size-4 mr-2" />
-                                                JSON
-                                            </MenubarItem>
-                                            <MenubarItem>
+                                            <MenubarItem onClick={onSaveText}>
                                                 <FileTextIcon className="size-4 mr-2" />
                                                 Text
                                             </MenubarItem>
