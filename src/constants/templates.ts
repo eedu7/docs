49da7a4bb56371d92dcd450e0662a5994917c928
eedu7 <mueedahmad067@gmail.com>
2025-04-05 17:26:09 +0500

    Added a array of all template gallery corosuel elements

---
+++ src/constants/templates.ts
@@ -0,0 +1,37 @@
+export const templates: {id: string, label: string, imageUrl: string}[] = [
+    {
+        id: "blank",
+        label: "Blank document",
+        imageUrl: "/documents/blank-document.svg",
+    },
+    {
+        id: "software-proposal",
+        label: "Software development proposal",
+        imageUrl: "/documents/software-proposal.svg",
+    },
+    {
+        id: "project-proposal",
+        label: "Project proposal",
+        imageUrl: "/documents/project-proposal.svg",
+    },
+    {
+        id: "business-letter",
+        label: "Business letter",
+        imageUrl: "/documents/business-letter.svg",
+    },
+    {
+        id: "resume",
+        label: "Resume",
+        imageUrl: "/documents/resume.svg",
+    },
+    {
+        id: "cover-letter",
+        label: "Cover letter",
+        imageUrl: "/documents/cover-letter.svg",
+    },
+    {
+        id: "letter",
+        label: "Letter",
+        imageUrl: "/documents/letter.svg",
+    },
+]
