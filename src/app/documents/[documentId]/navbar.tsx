cac41b6b261b4b313d15f67872e86e0c11bb9869
eedu7 <mueedahmad067@gmail.com>
2025-04-05 12:26:38 +0500

    Added the navbar

---
+++ src/app/documents/[documentId]/navbar.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
+export const Navbar = () => {
+    return (
+        <nav className="flex items-center justify-between">
+            <div className="flex gap-2 items-center">
+                <Link href="/">
+                    <Image src="/logo.svg" alt="Logo" width={36} height={36}/>
+                </Link>
+                <div className="flex flex-col">
+                    {/* TODO: DocumentInput */}
+                    {/* TODO: Menubar */}
+                </div>
+            </div>
+        </nav>)
+}
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import {Editor} from "@/app/documents/[documentId]/editor";
 import {Toolbar} from "@/app/documents/[documentId]/toolbar";
+import {Navbar} from "@/app/documents/[documentId]/navbar";
 
 interface DocumentIDPageProps {
     props: Promise<{ documentId: string }>
 }
 
 function DocumentIDPage({props}: DocumentIDPageProps) {
-    return (<div className="min-h-screen bg-[#FAFBFD]">
-        <Toolbar/>
-        <Editor/></div>)
+    return (
+        <div className="min-h-screen bg-[#FAFBFD]">
+            <Navbar />
+            <Toolbar />
+            <Editor />
+        </div>)
 }
 
 export default DocumentIDPage
