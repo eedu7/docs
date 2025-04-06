9324a41e4d735ddceef5338b670b3bb41fd5f919
eedu7 <mueedahmad067@gmail.com>
2025-04-06 07:56:01 +0500

    Added the ConvexClientProvidere with clerk:

--- src/components/ConvexClientProvider.tsx
+++ src/components/ConvexClientProvider.tsx
@@ -1,10 +1,30 @@
 "use client";
 
-import { ReactNode } from "react";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import {ReactNode} from "react";
+import {ConvexReactClient, Authenticated, Unauthenticated, AuthLoading} from "convex/react";
+import {ConvexProviderWithClerk} from "convex/react-clerk";
+import {ClerkProvider, useAuth, SignIn} from "@clerk/clerk-react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-export function ConvexClientProvider({ children }: { children: ReactNode }) {
-    return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+export function ConvexClientProvider({children}: { children: ReactNode }) {
+    return (
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
+            <ConvexProviderWithClerk
+                useAuth={useAuth}
+                client={convex}
+            >
+                <Authenticated>
+                    {children}
+                </Authenticated>
+                <Unauthenticated>
+                    <div className="flex justify-center mt-24">
+                        <SignIn />
+                    </div>
+                </Unauthenticated>
+                <AuthLoading>
+                    <p>Loading auth...</p>
+                </AuthLoading>
+            </ConvexProviderWithClerk>;
+        </ClerkProvider>)
 }
