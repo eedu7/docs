a72efa095bbe1e2b9171328cef72086ce69ad6ea
eedu7 <mueedahmad067@gmail.com>
2025-04-06 09:49:17 +0500

    Added the table and document row showing the documents

--- src/app/(home)/documents-table.tsx
+++ src/app/(home)/documents-table.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import {Doc} from "../../../convex/_generated/dataModel";
 import {PaginationStatus} from "convex/react";
-
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
+import {LoaderIcon} from "lucide-react";
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
+import {DocumentRow} from "@/app/(home)/document-row";
 
 
 interface DocumentsTableProps {
@@ -20,7 +14,40 @@ interface DocumentsTableProps {
 }
 
 export const DocumentsTable = ({documents, loadMore, status}: DocumentsTableProps) => {
-    return (
-        <div>DocumentsTable</div>
-    )
+    return (<div className="mx-auto max-w-screen-xl px-16 py-6 flex flex-col gap-5">
+            {documents === undefined ? (
+                <div className="flex items-center justify-center h-24">
+                    <LoaderIcon className="animate-spin text-muted-foreground size-5" />
+                </div>
+            ) : (
+                <Table>
+                    <TableHeader>
+                        <TableRow className="hover:bg-transparent border-none">
+                            <TableHead>Name</TableHead>
+                            <TableHead>&nbsp;</TableHead>
+                            <TableHead className="hidden md:table-cell">Shared</TableHead>
+                            <TableHead className="hidden md:table-cell">Created at</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    {documents.length === 0 ? (
+                        <TableBody>
+                            <TableRow className="hover:bg-transparent">
+                                <TableCell colSpan={4} className="h-24 text-center text-muted-foreground">
+                                    No documents found
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    ): (
+                        <TableBody>
+                            {documents.map((document) => (
+                                <DocumentRow
+                                    key={document._id}
+                                    document={document}
+                                />
+                            ))}
+                        </TableBody>
+                    )}
+                </Table>
+            )}
+        </div>)
 }
