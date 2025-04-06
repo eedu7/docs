4a7101b950a8150dee16be9a8d6e85cff85d91e4
eedu7 <mueedahmad067@gmail.com>
2025-04-06 17:01:28 +0500

    modifed the room

--- src/Room.tsx
+++ src/Room.tsx
@@ -11,7 +11,10 @@ import {useParams} from "next/navigation";
 export function Room({ children }: { children: ReactNode }) {
     const params = useParams();
     return (
-        <LiveblocksProvider publicApiKey={"pk_dev_plwjRN_BBjfUBuOjPtvnaeVNJmfVJT_LMc0ccDDrmXMdxkzPMZtV2pEqjVpTsq4Q"}>
+        <LiveblocksProvider
+            throttle={16}
+            authEndpoint="/api/liveblocks-auth"
+        >
             <RoomProvider id={params.documentId as string}>
                 <ClientSideSuspense fallback={<div>Loading…</div>}>
                     {children}
